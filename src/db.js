6692b38844f8e5a769fc5551ee296a93d1313105
AbelGod27 <babelv270@gmail.com>
2026-05-05 23:36:04 -0600

    Se modifico el env

--- src/db.js
+++ src/db.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
 const mysql = require("mysql2");
 
-//conexion con la bd
+// conexión con la bd
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 // conectar
 db.connect(err => {
     if (err) {
-    console.error("Error de conexión:", err);
+        console.error("Error de conexión:", err);
     } else {
-    console.log("Conectado a MySQL");
+        console.log("Conectado a Railway 🚀");
     }
 });
 
